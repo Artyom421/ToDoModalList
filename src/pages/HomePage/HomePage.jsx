01bed0c8d800c8579d7d9236d8acdecace9e7ec7
Artyom421 <artyom.pilot421@gmai.com>
2022-02-19 19:35:55 +0400

    bug fixed 2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -15,19 +15,12 @@ export const HomePage = () => {
     const { click } = state
     const dispatch = useDispatch()
 
-    const click1 = (result) => {
-        dispatch(click_1(result))
-    }
-    const click2 = (result) => {
-        dispatch(click_2(result))
-    }
-
     return (
         <div>
             <h1>This is HomePage</h1>
             <h2 onClick={() => goToToDoListPage("/ToDoListPage")} style={{cursor:"pointer"}}>GO TO ToDoList</h2>
-            <button onClick={() => click1('click-1')}>click 1</button>
-            <button onClick={() => click2('click-2')}>click 2</button>
+            <button onClick={() => dispatch(click_1("click-1"))}>click 1</button>
+            <button onClick={() => dispatch(click_2("click-2"))}>click 2</button>
             <p>{click}</p>
         </div>
     )
